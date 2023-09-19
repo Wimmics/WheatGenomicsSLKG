45bf96acccb757cf133b134268cd6ff27b3737b6
Franck Michel <frmichel@users.noreply.github.com>
2023-09-19 09:12:37 +0200

    Refactoring

---
+++ dataset/start_virtuoso.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# To be executed as sudo
+
+if [ -f "env.sh" ]; then
+    echo "Running env.sh."
+    . ./env.sh
+fi
+
+docker run --name $VIRTUOSO_CONTAINER \
+  -p 8892:8890 -it \
+  -e DBA_PASSWORD=$VIRTUOSO_PWD \
+  -e SPARQL_UPDATE=true \
+  -e DEFAULT_GRAPH=http://ns.inria.fr/d2kab/graph/wheatgenomicsslkg \
+  -v /home/yacoubi/d2kab/virtuosodb:/database \
+  -v /home/yacoubi/d2kab/WheatGenomicsSLKG/dataset:/dataset_wheatgenomicslkg \
+  -v /home/yacoubi/d2kab/RiceGenimocsSLKG/dataset:/dataset_ricegenomicslkg \
+  -d openlink/virtuoso-opensource-7:latest
